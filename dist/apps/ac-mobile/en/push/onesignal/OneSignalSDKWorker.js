76b4c766542b8e62579fee952b7785020bbca498
JesusChuz <j.mendezquesada18@gmail.com>
2023-09-22 09:02:53 -0600

    first commit

---
+++ dist/apps/ac-mobile/en/push/onesignal/OneSignalSDKWorker.js
@@ -0,0 +1,2 @@
+// KEEP Until 2024-03-09: Required for pushes to work correctly for users who have not re-visited to migrate to the new OneSignal ServiceWorker file located at UPDATE YOUR URL HERE: http://localhost:4200/push/onesignal/OneSignalSDKWorker.js.
+self.importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
